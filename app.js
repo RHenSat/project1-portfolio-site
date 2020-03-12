8f51efab9282417a30a8c7eeddf991e8672c4ea2
R. Sebastien Henri-Saturne <sebastien@Rs-MacBook-Air.local>
2020-03-12 10:25:57 -0400

    my repo

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log('hello!')
+
+
+/*let url = 'https://docs.google.com/spreadsheets/d/1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA/edit#gid=0'
+
+let id= '1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA'
+
+let source = https://spreadsheets.google.com/feeds/list/1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA/od6/public/values?alt=json
+
+fetch(source)
+    .then( response => response.json())
+    .then(data => console.log('data',data))
+
+let projects = [
+    {title: 'Fashion blog', image: 'image url', description: "the description",'url to project'}
+]/*
+
+
+
+
+/*let main = document.getElementById("main")
+Bla.forEach(element,index)
+let card = 
+<div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+
+
+console.log (card)
+const devWork = document.creatElement ('div')*/
