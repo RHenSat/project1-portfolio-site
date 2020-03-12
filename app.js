443eedf8d178fb42dab9affffd42a429047062b8
R. Sebastien Henri-Saturne <sebastien@Rs-MacBook-Air.local>
2020-03-12 18:33:22 -0400

    getting there

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 console.log('hello!')
 
 
-/*let url = 'https://docs.google.com/spreadsheets/d/1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA/edit#gid=0'
+let source =`https://spreadsheets.google.com/feeds/list/1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8/od6/public/values?alt=json`
+/*let source = 'https://docs.google.com/spreadsheets/d/1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8/edit#gid=0'*/
 
-let id= '1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA'
+let id= '1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8'
 
-let source = https://spreadsheets.google.com/feeds/list/1gy2-fuf91TCfGzvyeud-crZnupk_3AreYD7rVWByTtA/od6/public/values?alt=json
+let projects = ['The Best Chocolate Chip Cookies','Fashion Blog','Random Imager','Curiosity']
 
 fetch(source)
-    .then( response => response.json())
-    .then(data => console.log('data',data))
+  .then( response => response.json() ) // this parses the data from string back into an object
+  .then( data =>  {
+      console.log('data', data)
 
-let projects = [
-    {title: 'Fashion blog', image: 'image url', description: "the description",'url to project'}
-]/*
+  let projects = data.feed.entry.map( project => {
+    return {
+        title: project.gsx$title.$t,
+        image: project.gsx$image.$t,
+        description: project.gsx$description.$t,
+        url: project.gsx$url.$t
+      }
+    })
+    createMyProjects(projects)
+}) 
+.catch( err => console.log('err', err))
+function createMyProjects(projects){
+    projects.forEach(project => {
+        let $card = $(`
+        <div class="col mb-4">
+      <div class="card">
+        <img src="${project.image}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">${project.title}</h5>
+          <p class="card-text">${project.description}</p>
+          <a href="${project.url} class="btn btn-primary">Button</a>
+        </div>
+      </div>
+    </div>`)
+    console.log(project.title)
 
+        $('.cards').append($card)
+})}
 
-
-
-/*let main = document.getElementById("main")
-Bla.forEach(element,index)
-let card = 
-<div class="card" style="width: 18rem;">
-<img src="..." class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
-
-
-console.log (card)
-const devWork = document.creatElement ('div')*/
