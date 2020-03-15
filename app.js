aa1d2a1797e9f59a5df0f05104d253deb94d2d7a
R. Sebastien Henri-Saturne <sebastien@Rs-MacBook-Air.local>
2020-03-15 16:46:10 -0400

    almost done

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ console.log('hello!')
 
 
 let source =`https://spreadsheets.google.com/feeds/list/1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8/od6/public/values?alt=json`
-/*let source = 'https://docs.google.com/spreadsheets/d/1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8/edit#gid=0'*/
+
 
 let id= '1WBpqh72EFYlK5pVZ2rXCjlribtDEV5x-vkl914I2gy8'
 
 let projects = ['The Best Chocolate Chip Cookies','Fashion Blog','Random Imager','Curiosity']
 
 fetch(source)
-  .then( response => response.json() ) // this parses the data from string back into an object
+  .then( response => response.json() ) 
   .then( data =>  {
       console.log('data', data)
 
@@ -27,7 +27,7 @@ fetch(source)
 function createMyProjects(projects){
     projects.forEach(project => {
         let $card = $(`
-        <div class="col mb-4">
+        
       <div class="card">
         <img src="${project.image}" class="card-img-top" alt="">
         <div class="card-body">
@@ -35,7 +35,6 @@ function createMyProjects(projects){
           <p class="card-text">${project.description}</p>
           <a href="${project.url} class="btn btn-primary">Button</a>
         </div>
-      </div>
     </div>`)
     console.log(project.title)
 
